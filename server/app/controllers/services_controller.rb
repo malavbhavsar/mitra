0e70f301d017abd8567cc18269bffd329fb3fbe3
Devika <s.devikanair@gmail.com>
2013-09-21 15:37:26 -0700

    Adding service controllers

--- server/app/controllers/services_controller.rb
+++ server/app/controllers/services_controller.rb
@@ -1,9 +1,18 @@
 class ServicesController < InheritedResources::Base
- # before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:all]
 
   #Currently uses all passed params, userid not associated
   def new
     @service = Service.new(params[:service])
+    @service.userid = current_user.id
     @service.save
+  end
+
+  def index
+    @services = Service.find_all_by_userid(current_user.id)
+  end
+
+  def all
+    @services = Service.all
   end
 end
